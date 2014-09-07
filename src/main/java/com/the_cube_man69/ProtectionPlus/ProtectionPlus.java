a4e4ff709e301fc31eff4f4174f847313146a0d3
Ivaz01 <ivanwaz@zoominternet.net>
2014-09-07 18:46:48 -0400

    Mod Class

---
+++ src/main/java/com/the_cube_man69/ProtectionPlus/ProtectionPlus.java
@@ -0,0 +1,31 @@
+package com.the_cube_man69.ProtectionPlus;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="ProtectionPlus", name="ProtectionPlus", version="1.7.10-1.0")
+public class ProtectionPlus
+{
+    @Mod.Instance("ProtectionPlus")
+    public static ProtectionPlus instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void Init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
