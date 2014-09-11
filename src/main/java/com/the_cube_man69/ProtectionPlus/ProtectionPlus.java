0514a737e24e3c36bc0b159796f4905e3e4b2bbd
Ivaz01 <ivanwaz@zoominternet.net>
2014-09-10 21:01:36 -0400

    configurations

--- src/main/java/com/the_cube_man69/ProtectionPlus/ProtectionPlus.java
+++ src/main/java/com/the_cube_man69/ProtectionPlus/ProtectionPlus.java
@@ -1,5 +1,6 @@
 package com.the_cube_man69.ProtectionPlus;
 
+import com.the_cube_man69.ProtectionPlus.Configuration.ConfigurationHandler;
 import com.the_cube_man69.ProtectionPlus.proxy.IProxy;
 import com.the_cube_man69.ProtectionPlus.reference.Reference;
 import cpw.mods.fml.common.Mod;
@@ -8,19 +9,20 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
+
 @Mod(modid= Reference.Mod_ID, name= Reference.Mod_Name, version= Reference.Version)
 public class ProtectionPlus
 {
     @Mod.Instance(Reference.Mod_ID)
     public static ProtectionPlus instance;
 
-    @SidedProxy(clientSide ="com.the_cube_man69.ProtectionPlus.proxy.ClientProxy", serverSide ="com.the_cube_man69.proxy.ServerProxy")
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
     public static IProxy proxy;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-
+        ConfigurationHandler.init(event.getSuggestedConfigurationFile());
     }
 
     @Mod.EventHandler
--- src/main/java/com/the_cube_man69/ProtectionPlus/reference/Reference.java
+++ src/main/java/com/the_cube_man69/ProtectionPlus/reference/Reference.java
@@ -5,4 +5,6 @@ public class Reference
     public static final String Mod_ID = "ProtectionPlus";
     public static final String Mod_Name = "Protection Plus";
     public static final String Version = "1.7.10-1.0";
+    public static final String CLIENT_PROXY_CLASS = "com.the_cube_man69.ProtectionPlus.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "com.the_cube_man69.ProtectionPlus.proxy.ServerProxy";
 }
